83cd2a156206fe2b9254669c184b4f24be6cca80
Zachary Klein <zachkklein@gmail.com>
2025-07-14 21:04:05 -0700

    tailwind

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -13,14 +13,16 @@ const Sidebar = ({user}: SiderbarProps) => {
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
-            <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
-            <Image src="/icons/logo.svg"
-                width={34}
-                height={34}
-                alt="logo"
-                className="size-[24px] max-xl:size-14"/>
-                <h1 className="sidebar-logo">Horizon</h1>
-            </Link>
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image 
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
 
             {sidebarLinks.map((item) => {
             const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`)
@@ -29,9 +31,21 @@ const Sidebar = ({user}: SiderbarProps) => {
                 <Link 
                 href={item.route} 
                 key={item.label}
-                className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+                className={cn('sidebar-link', { 'bg-gradient-to-r from-[#0179FE] to-[#4893FF]': isActive })}
                 >
-                {item.label}
+                <div className="relative size-6">
+                    <Image 
+                    src={item.imgURL}
+                    alt={item.label}
+                    fill
+                    className={cn({
+                        'brightness-[3] invert-0': isActive
+                    })}
+                    />
+                </div>
+                <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                  {item.label}
+                </p>
                 </Link>
                 );
             })}
